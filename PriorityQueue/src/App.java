ec19bfb5e75c204d2bb31e2c5c5f4801398ab99a
clide7029 <skylerwb7029@gmail.com>
2021-11-30 18:10:30 -0800

    update

--- PriorityQueue/src/App.java
+++ PriorityQueue/src/App.java
@@ -1,11 +1,11 @@
 import java.util.Random;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         PriorityQueue pq = new PriorityQueue();
         Random rand = new Random();
 
-        for(int e=0; e<3; e++){
+        for (int e = 0; e < 3; e++) {
             int p = rand.nextInt(100);
             System.out.println("pushing on (" + p + ", " + e + ")");
             pq.push(p, e);
